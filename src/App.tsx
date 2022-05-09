0d0ca7b8a0d58b07eb073b524f977500a271c163
Brunno Costa <brunnogpcosta@gmail.com>
2022-05-09 20:20:33 -0300

    Criando componente Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-  
-      <>
-      <Header></Header>
-      <h1>Hello</h1>
+
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
