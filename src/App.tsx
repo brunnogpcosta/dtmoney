258ba3cd010a2e8d205272ea4194994251c8c7b1
Brunno Costa <brunnogpcosta@gmail.com>
2022-05-09 19:49:25 -0300

    criando componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
+  
+      <>
+      <Header></Header>
       <h1>Hello</h1>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
